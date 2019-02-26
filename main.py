8446e67240c5eb08fb64bb06dfff4f5348732265
suulcoder <ef.proyectomundial@gmail.com>
2019-02-25 23:17:49 -0600

    Container

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ Simulador de procesos en una computadora
 """
 import random
 import simpy
+import math
 
 datos = []
-
 class computador(object):
 
     def __init__(self, env, procesadores, memoria):
@@ -20,17 +20,17 @@ class computador(object):
         # Tiempo de creacion
         self.env = env
         self.cpu = simpy.Resource(env, capacity=procesadores)
-        self.ram = simpy.Container(env, init=memoria, capacity=memoria)
+        self.ram = simpy.Container(env, init=0, capacity=memoria)
 
 
 class Proceso(object):
 
-    def __init__(self, env, procesos, ejecutar, memoria, computador, nombre):
+    def __init__(self, env, procesos, ejecutar, memoria, computador, nombre, inicial):
         self.env = env
-        self.action = env.process(self.run(env, ejecutar, memoria, computador, nombre))
+        self.action = env.process(self.run(env, ejecutar, memoria, computador, nombre, inicial))
         self.procesos = procesos
 
-    def run(self, env, ejecutar, memoria, computador, nombre):
+    def run(self, env, ejecutar, memoria, computador, nombre, inicial):
         with computador.cpu.request() as process:  # Utiliza el CPU
             contador = 3
             while True:
@@ -38,66 +38,74 @@ class Proceso(object):
                     contador = contador - 1
                     self.procesos = self.procesos - 1
                     yield self.env.timeout(1)
-                    if (self.procesos == 0):  # Si ya no existe mas instrucciones
-                        env.process(terminar(env, memoria, computador, nombre))  # Termina
                 except simpy.Interrupt:
                     if (self.procesos < 0):  # Si ya no existe mas instrucciones
-                        env.process(terminar(env, memoria, computador, nombre))  # Termina
+                        env.process(terminar(env, memoria, computador, nombre, inicial))  # Termina
                     else:
-                        print('%s a procesado 3 instrcciones a %s. Instrucciones restantes:%s' % (nombre, env.now, self.procesos))
+                        print('%s a procesado 3 instrucciones a %s. Instrucciones restantes:%s' % (nombre, env.now, self.procesos))
                         continuar = random.randint(1, 2)
                         if continuar == 0:  # Si regresa a ready
-                            yield env.process(ready(env, self.procesos, ejecutar, memoria, computador, nombre))
+                            yield env.process(ready(env, self.procesos, ejecutar, memoria, computador, nombre, inicial))
                         elif continuar == 1:  # Si debe esperar
-                            yield env.process(wait(env, self.procesos, ejecutar, memoria, computador, nombre))
+                            yield env.process(wait(env, self.procesos, ejecutar, memoria, computador, nombre, inicial))
 
 
-def new(env, instrucciones, intervalo, cantidad, computador):
-    for i in range(cantidad):
+def new(env, instrucciones, intervalo, cantidad, computador, inicio):
+    for i in range(inicio, cantidad):
         tiempo = random.expovariate(1.0 / intervalo)#Generamos el intervalo
         memoria_RAM = random.randint(1, 10)#Generamos la memoria ram que consume el proceso
         instructions = random.randint(1, 10)#Generamos las instrucciones que consume el proceso
-        if (computador.ram.capacity != 0):
-            env.timeout(tiempo)
-            computador.ram.get(memoria_RAM)
-            yield env.process(ready(env, instructions, instrucciones, memoria_RAM, computador, 'Proceso %d' % i))
+        inicial = env.now
+        if (computador.ram.level<computador.ram.capacity):
+            yield env.timeout(1)
+            yield computador.ram.put(memoria_RAM)
+            yield env.process(ready(env, instructions, instrucciones, memoria_RAM, computador, 'Proceso %d' % i, inicial))
+        else:
+            yield env.timeout(1)
+            yield env.process(new(env,instructions,intervalo,cantidad,computador,i))
 
 
-def ready(env, procesos, ejecutar, memoria, computador, nombre):
+def ready(env, procesos, ejecutar, memoria, computador, nombre, inicial):
     print('%s preparando proceso a %s' % (nombre, env.now))
-    proceso = Proceso(env, procesos, ejecutar,memoria, computador, nombre)
+    proceso = Proceso(env, procesos, ejecutar,memoria, computador, nombre, inicial)
     yield env.process(correr(env, ejecutar, proceso))
 
 def correr(env, ejecutar, proceso):
     yield env.timeout(ejecutar)
     proceso.action.interrupt()
 
-def wait(env, procesos, ejecutar, memoria, computador, nombre):
+def wait(env, procesos, ejecutar, memoria, computador, nombre, inicial):
     yield env.timeout(1)  # Wait por una unidad de tiempo
     print('%s en espera a %s' % (nombre, env.now))
-    env.process(ready(env, procesos, ejecutar, memoria, computador, nombre))  # Ejecutar ready de nuevo hasta que se terminen los procesos
+    env.process(ready(env, procesos, ejecutar, memoria, computador, nombre, inicial))  # Ejecutar ready de nuevo hasta que se terminen los procesos
 
-def terminar(env,memoria,computador,nombre):
+def terminar(env,memoria,computador,nombre, inicial):
     print('%s terminado a %s' % (nombre, env.now))
-    datos.append(env.now)  # Almacenamos la informacion para realizar pruebas estadisticas
-    yield computador.ram.put(memoria)
+    datos.append(env.now-inicial)  # Almacenamos la informacion para realizar pruebas estadisticas
+    yield computador.ram.get(memoria)
 
 
 #-----------------------------------------------------------------------
 #Definimos los valores predeterminados
-procesadores = 1#Numero de  procesadores en la computadora
+procesadores = 2#Numero de  procesadores en la computadora
 memoria = 100#Cantidad de memoria RAM de la computadora
 instrucciones = 3#Cantidad de instrucciones por proceso
-intervalo = 10#Intervalo de aparacion de procesos
+intervalo = 1#Intervalo de aparicion de procesos
 cantidad = 50#Cantidad de procesos generados
 
 env = simpy.Environment()
-random.seed(10)
+random.seed(100)
 computador = computador(env, procesadores, memoria)
-process_gen = env.process(new(env, instrucciones, intervalo, cantidad, computador))
-env.run(until=None)
+process_gen = env.process(new(env, instrucciones, intervalo, cantidad, computador,0))
+env.run(until=process_gen)
 total = 0
-print(datos)
-for i in datos:
+for i in datos:#Calculamos el promedio
     total = total + i
-print("El tiempo promedio es " + str(total / len(datos)))
+promedio =(total / len(datos))
+sumatoria = 0
+for i in datos:#Calculamos la desviacion estandar
+    sumatoria = sumatoria + ((i-promedio)**2)
+desviacion = sumatoria/len(datos)
+desviacion = math.sqrt(desviacion)
+print("El tiempo promedio es " + str(promedio))
+print("La desviacion estandar es " + str(desviacion))
